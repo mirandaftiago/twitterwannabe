55c91a5594e24738b242af849add3b64a17e7ada
Tiago Miranda <miranda.f.tiago@gmail.com>
2021-03-30 23:07:47 +0100

    added enableEdit to edit button

--- src/app.js
+++ src/app.js
@@ -10,6 +10,9 @@ document.querySelector('.post-submit').addEventListener('click', submitPost);
 //Listen for delete
 document.querySelector('#posts').addEventListener('click', deletePost);
 
+//Listen for edit state
+document.querySelector('#posts').addEventListener('click', enableEdit);
+
 //Get Posts
 function getPosts() {
   http.get('http://localhost:3000/posts')
@@ -40,7 +43,6 @@ function submitPost() {
 
 //Delete Post
 function deletePost(e) {
-  e.preventDefault();
 
   if(e.target.parentElement.classList.contains('delete')) {
     const id = e.target.parentElement.dataset.id;
@@ -53,4 +55,24 @@ function deletePost(e) {
         .catch(err => console.log(err));
     } 
   }
+    e.preventDefault();
+}
+
+//Enable Edit State
+function enableEdit(e) {
+  
+  if(e.target.parentElement.classList.contains('edit')) {
+    const id = e.target.parentElement.dataset.id;
+    const title = e.target.parentElement.previousElementSibling.textContent;
+    const body = e.target.parentElement.previousElementSibling.textContent;
+    
+    const data = {
+      id,
+      title,
+      body
+    }
+    console.log(data)
+  }
+  
+  e.preventDefault();
 }
